9bde5a5ecf003ef11a6079ab1ee8aa1b52480e4b
+ <+>
2021-08-24 14:25:24 -0700

    messing up the renderFeature function

--- index.js
+++ index.js
@@ -40,14 +40,31 @@ function renderExercises(obj) {
     })
 })
 
-const li = document.getElementsByName('li')
-
-li.addEventListener('click', e => {
-    const featureForm = document.querySelector('form#feature')
-    const featureImg = document.createElement('img')
-    const featureName = document.createElement('h3')
-    const featureTarget = document.createElement('h4')
+function renderFeatured (obj) {
     
-    featureImg.src = obj.gif
-    featureForm.append(featureImg)
-})
+    const li = document.getElementsByName('li')
+    li.addEventListener('click', e =>{
+        const feature = document.getElementsByName('feature')
+        const featureTarget = document.createElement('h3')
+        const featureName = document.createElement('h4')
+        const featureGif = document.createElement('img')
+        const featureEquip = document.createElement('p')
+
+        featureTarget.textContent = obj.target
+        featureName.textContent = obj.name
+        featureGif.src = obj.gif
+        featureEquip.textContent = obj.type
+
+        feature.append(featureTarget, featureName, featureGif, featureEquip)
+
+        fetch(`https://exercisedb.p.rapidapi.com/exercises/target/${submitValue}`, {
+            "method": "GET",
+               "headers": {
+                   "x-rapidapi-host": "exercisedb.p.rapidapi.com",
+                   "x-rapidapi-key": "9074bf701emsh2b8696dac91ac18p161ae0jsn7153acb84d2d"
+               }
+            })
+            .then(resp => resp.json())
+            .then(arr => console.log(arr))
+         })
+        }
