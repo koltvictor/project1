8cc64e2d623a229a3947e6adad5376ef0df12448
+ <+>
2021-08-24 13:10:40 -0700

    editing HTML and adding some JS for FEATURED

--- index.js
+++ index.js
@@ -1,36 +1,18 @@
 // JS GOES HERE //
 
-const button = document.getElementById('submitButton')
-const list = document.getElementById('fetchedData')
-
 const APIURL = "https://exercisedb.p.rapidapi.com/exercises"
 
-// fetch(APIURL, {
-//    "method": "GET",
-//    "headers": {
-//        "x-rapidapi-host": "exercisedb.p.rapidapi.com",
-//        "x-rapidapi-key": "9074bf701emsh2b8696dac91ac18p161ae0jsn7153acb84d2d"
-//    }
-// })
-// .then(resp => resp.json())
-// .then(arr => {
-//     arr.forEach(obj => {
-//         renderExercises(obj)
-// });
-// })
-
-// user searches api for exercises
-// only pull like matches
-
-
+const button = document.getElementById('submitButton')
+const list = document.getElementById('fetchedData')
+const routine = document.getElementById('routine')
+const searchForm = document.getElementById('search-form')
 
 function renderExercises(obj) {
     const newExercise = document.createElement('li')
     newExercise.textContent = obj.name
     list.append(newExercise)
-
 }
-const searchForm = document.getElementById('search-form')
+
    
     searchForm.addEventListener('submit', event => {
         event.preventDefault()
@@ -57,3 +39,23 @@ const searchForm = document.getElementById('search-form')
      })
     })
 })
+
+function featureExercise (obj) {
+    
+    const featureImg = document.createElement('img')
+    const featureName = document.createElement('h3')
+    const featureTarget = document.createElement('p')
+    const featureEquipment = document.createElement('p')
+    const exerciseList = document.getElementsByName('li')
+
+
+    featureImg.src = obj.url
+    featureName.textContent = obj.name
+    featureTarget.textContent = obj.target
+    featureEquipment.textContent = obj.equipment
+
+    exerciseList.addEventListener('click', e => {
+        featureImg.src = obj.gif
+    })   
+
+}
